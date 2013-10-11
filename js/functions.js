74ffcbe5d79830407649cd0622ee9c6627b4b381
Josh Lloyd <j.nevercast@gmail.com>
2013-10-11 17:49:09 +1300

    That's All for today

--- js/functions.js
+++ js/functions.js
@@ -25,3 +25,34 @@ function scrollToTop() {
 		'scrollTop':0
 	}, 500);
 }
+
+function Zone( zoneName, zoneStatusMessage, zoneProgress ) {
+	this.name = zoneName;
+	this.status = zoneStatusMessage;
+	this.progress = zoneProgress;
+}
+
+Zone.prototype.getName = function() {
+	return this.name;
+}
+
+Zone.prototype.getStatus = function() {
+	return this.status;
+}
+
+Zone.prototype.getProgress = function() {
+	return this.progress;
+}
+
+var database = {}
+
+function updateZoneGUI() {
+	
+}
+
+function updateZones() {
+	database.zones = []
+	database.zones[0] = new Zone('Garden', 'completed', 100);
+	database.zones[1] = new Zone('Crops', 'running', 40);
+	updateZoneGUI();
+}
